54c609ca25a93a9bc89c9fdc4d3f4d826d5f959a
Serendipity <112377744+39998@users.noreply.github.com>
2023-12-22 20:36:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+apt install mailutils -y
+apt install ssmtp -y
 read -p "Admin:" txt
 sed -i "23s/.\{5\}/&$txt/" /file/ssmtp.conf
 sed -i "8s/.\{5\}/&$txt/"/file/revaliases
+echo "echo | mail -s "test" xx@segfault.net"
